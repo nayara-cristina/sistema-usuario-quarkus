ca0960ebc8d13fd3f96954cad49760c4abcd55ee
nayara-cristina <nayara.cristina.jesus@ibm.com>
2023-08-28 14:24:30 -0300

    Criacao do sistema de gerenciamento de usuarios

---
+++ src/main/java/io/sistema/sistemausuarioquarkus/rest/UserResource.java
@@ -0,0 +1,60 @@
+package io.sistema.sistemausuarioquarkus.rest;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.sistema.sistemausuarioquarkus.domain.model.User;
+import io.sistema.sistemausuarioquarkus.rest.dto.CreateUserRequest;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class UserResource{
+    @POST
+    @Transactional
+    public Response createUser(CreateUserRequest userRequest){
+        User user = new User();
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+
+        user.persist();
+
+        return Response.ok(user).build();
+    }
+
+    @GET
+    public Response listAllUsers(){
+        PanacheQuery<PanacheEntityBase> query = User.findAll();
+        return Response.ok(query.list()).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deleteUser(@PathParam("id") Long id){
+        User user = User.findById(id);
+
+        if(user != null){
+            user.delete();
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData){
+        User user = User.findById(id);
+        if(user != null){
+            user.setName(userData.getName());
+            user.setAge(userData.getAge());
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+}
